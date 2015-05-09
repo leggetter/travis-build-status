46222795a7a46b861587ce5bb6f70650225ecbcf
leggetter <phil@leggetter.co.uk>
2015-05-09 21:58:07 +0100

    Remove console.log

--- components/TemplateParser.js
+++ components/TemplateParser.js
@@ -6,7 +6,6 @@ TemplateParser.prototype.parse = function(templateStr, tokens) {
   for(var token in tokens) {
     console.log(token, tokens[token]);
     regex = new RegExp('{{' + token + '}}', 'g');
-    console.log(regex);
     templateStr = templateStr.replace(regex, tokens[token]);
   }
   return templateStr;
